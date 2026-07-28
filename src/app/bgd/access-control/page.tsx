e9ed61edb19fa6744698a768a3c854b0dcf7e3f9
ngly29 <lynguyenngoc007@gmail.com>
2026-07-28 16:34:17 +0700

    feat: mock data access control

---
+++ src/app/bgd/access-control/page.tsx
@@ -0,0 +1,41 @@
+"use client";
+
+import styles from "./page.module.css";
+import Button from "@/components/shared/Button/Button";
+import {useState} from "react";
+import PermissionModal from "@/components/bgd/AccessControl/PermissionModal";
+import AccountList from "@/components/bgd/AccessControl/AccountList";
+
+export default function AccessControlPage() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const handleSave = (data: any) => {
+    console.log("Saved permissions:", data);
+    alert("Lưu phân quyền thành công!");
+  };
+  return (
+    <div className={styles.page}>
+      {/* Header */}
+      <div className={styles.header}>
+        <div>
+          <h1 className={styles.title}>Quản lý phân quyền và kiểm soát</h1>
+          <p className={styles.subtitle}>Quản lý phân quyền người dùng</p>
+        </div>
+        <div className={styles.actions}>
+          <Button variant="primary" onClick={() => setIsModalOpen(true)}>
+            Tạo phân quyền
+          </Button>
+        </div>
+      </div>
+
+      {/* table */}
+      <AccountList/>
+
+      {/* Modal */}
+      <PermissionModal
+        isOpen={isModalOpen}
+        onClose={() => setIsModalOpen(false)}
+        onSave={handleSave}
+      />
+    </div>
+  );
+}
